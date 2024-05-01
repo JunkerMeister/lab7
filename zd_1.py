c892f61c59caaf96e654f4ce4f35552ab15f955d
Vova <your_email@whatever.com>
2024-05-01 15:47:03 +0300

    -_--_--_-

---
+++ zd_1.py
@@ -0,0 +1,15 @@
+def count_ways_to_climb(n):
+    if n == 1:
+        return 1
+    if n == 2:
+        return 2
+
+    dp = [0] * (n + 1)
+    dp[1] = 1
+    dp[2] = 2
+
+    for i in range(3, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 2]
+
+    return dp[n]
+print(count_ways_to_climb(int(input('enter count of ways to climb: '))))
